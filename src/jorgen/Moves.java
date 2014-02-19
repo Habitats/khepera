43af0af406e018d8af73efb380795f9fd082f27a
Jørgen Grimnes <jorgenkg@gmail.com>
2014-02-19 11:42:47 +0100

    oving 2 koden

---
+++ src/jorgen/Moves.java
@@ -0,0 +1,30 @@
+
+public enum Moves{
+		LEFT,
+		RIGHT,
+		TURN_AROUND,
+		FORWARD,
+		FORWARDSLOW,
+		BACKWARD,
+		START,
+		END, 
+		BACKWARDSLOW, POINT;
+		
+		private Moves opposite;
+
+	    static {
+	    	START.opposite = END;
+	    	END.opposite = START;
+	    	LEFT.opposite = RIGHT;
+	        RIGHT.opposite = LEFT;
+	        FORWARD.opposite = FORWARD;
+	        FORWARDSLOW.opposite = FORWARDSLOW;
+	        BACKWARDSLOW.opposite = BACKWARDSLOW;
+	        POINT.opposite = POINT;
+	        TURN_AROUND.opposite = TURN_AROUND;
+	    }
+	    
+	    public Moves getBacktrackMove() {
+	        return this.opposite;
+	    }
+	}
