74841c4398831801be591d3bc9828b6920039912
Jørgen Grimnes <jorgenkg@gmail.com>
2014-03-15 16:48:26 +0100

    New feature: correctly assign wall distance intervals to measurements
    Minor:
    + Added a private SensorInterval.class
    + Adjusted the sensor intervals according to more accurate observations
    + Implemented a fast distance check

--- src/khepera/managers/SensorManager.java
+++ src/khepera/managers/SensorManager.java
@@ -8,6 +8,29 @@ public class SensorManager{
 	
 	RobotController controller; // 
 	
+	/* Discretizing the sensor data */
+	private class SensorInterval {
+		int xFar, xClose;
+		double sensorFar, sensorNear;
+		public SensorInterval(double sensorFar, double sensorNear, int xFar, int xClose){
+			this.sensorFar = sensorFar;
+			this.sensorNear = sensorNear;
+			this.xFar = xFar;
+			this.xClose = xClose;
+		}
+		public boolean inSensorInverval( int sensorValue ){ return (this.sensorNear<sensorValue && sensorValue<=this.sensorFar); }
+		public int[] getDistanceInterval(){ return new int[]{this.xFar, this.xClose}; }
+	}
+	SensorInterval[] discreteSensorIntervals = new SensorInterval[]{ 
+			new SensorInterval(10,17.5,345,303), 
+			new SensorInterval(17.5, 40, 302, 274), 
+			new SensorInterval(40, 84, 273, 260),
+			new SensorInterval(84, 150, 259, 217),
+			new SensorInterval(150, 300, 216, 174),
+			new SensorInterval(300, 540, 173, 130),
+			new SensorInterval(540, 800, 129, 102),
+			new SensorInterval(800, 2000, 101, 0),
+		};
 	
 	
 	public SensorManager( RobotController controller ){
@@ -16,17 +39,20 @@ public class SensorManager{
 	}
 
 	
-	public int getSensorData( int sensorIndex ){
+	public int[] getDistanceRange( int sensorIndex ){
 		/* Observation:
 		 * Due to the noise implementation, we should only perform a single sensor measure
 		 * and categorize the distance interval. (According to statistical analysis)
 		 */
-			
+		int distanceValue = this.controller.getDistanceValue( sensorIndex );
+		
+		for( int i=0; i<this.discreteSensorIntervals.length; i++ ){
+			// Loop over the discrete distance sensor intervals
+			if( this.discreteSensorIntervals[i].inSensorInverval( distanceValue ) ){
+				// If the measured distance may appear in this range interval relative to the wall
+				return this.discreteSensorIntervals[i].getDistanceInterval();
+			}
+		}
 	}
 	
-	private Point calculateInterval( int s ){
-		int[] sensorMeans = new int{15, 20, 70, 100, 200, 430, 675, 950, 1023};
-		int[] 
-		if( )
-	}
 }
