64a0ccc67d54f1fdaa17cd0232acac09d21d0e76
Habitats <mail@habitats.no>
2014-03-31 16:13:18 +0200

    fixed imports

--- src/khepera/behaviour/MoveToLightBehaviour.java
+++ src/khepera/behaviour/MoveToLightBehaviour.java
@@ -2,7 +2,6 @@ package khepera.behaviour;
 
 import khepera.managers.MovementManager;
 import khepera.managers.SensorManager;
-import khepera.state.CollisionSense;
 import khepera.state.FindNearestLight;
 import khepera.state.Move;
 import khepera.state.Turn;
