823e70ee9951b744c5b56f9a4d1f5d7832323ef9
Habitats <mail@habitats.no>
2014-03-31 15:32:30 +0200

    added javadoc

--- src/khepera/AbstractController.java
+++ src/khepera/AbstractController.java
@@ -9,6 +9,14 @@ import khepera.managers.MovementManager;
 import khepera.managers.SensorManager;
 import edu.wsu.KheperaSimulator.RobotController;
 
+/**
+ * Abstract controller class that is to be extended by any specific controller implementation. This
+ * class extends the basic functionality of the default khepera controller to suit and manage our
+ * own achitectual properties.
+ * 
+ * @author Olav, Patrick 
+ * 
+ */
 public abstract class AbstractController extends RobotController {
   protected SensorManager sensorManager;
   protected MovementManager movementManager;
@@ -16,18 +24,18 @@ public abstract class AbstractController extends RobotController {
   private Behaviour lastRunBehaviour = null;
   private ArrayList<Behaviour> behaviours;
   private boolean initialized = false;
-  
+
   public AbstractController() {
     startTime = System.currentTimeMillis();
     sensorManager = new SensorManager(this);
     movementManager = new MovementManager(this);
     behaviours = new ArrayList<Behaviour>();
-    lastRunBehaviour = new CollisionRecovery(1999999999, sensorManager, movementManager); 
+    lastRunBehaviour = new CollisionRecovery(1999999999, sensorManager, movementManager);
     addBehaviour(lastRunBehaviour);
-    
+
     addBehaviours();
-   
-    //Start the sensor manager
+
+    // Start the sensor manager
     Thread t = new Thread(sensorManager);
     t.setName("SensorManager thread");
     t.start();
@@ -36,50 +44,56 @@ public abstract class AbstractController extends RobotController {
 
   @Override
   public void doWork() throws Exception {
-	  if (!initialized) return;
-	  updateStatus();
+    if (!initialized)
+      return;
+    updateStatus();
     runBehaviour();
   }
-  
+
   /**
    * This method should use the addBehaviour() function to add all wanted behaviors
    */
   protected abstract void addBehaviours();
-  
+
   /**
-   * Adds a behaviour to the controller. The priority of the behaviour should not exceed 2'000'000'000.
+   * Adds a behaviour to the controller. The priority of the behaviour should not exceed
+   * 2'000'000'000.
+   * 
    * @param b
    */
   protected void addBehaviour(Behaviour b) {
-	  behaviours.add(b);
-	  Collections.sort(behaviours);
-  }
-  
-  private void runBehaviour() {
-	  if (behaviours.size() == 0) {
-		  Logger.getInstance().error("No behaviours added to the controller...");
-		  return;
-	  } 
-	  
-	  for(Behaviour b : behaviours) {
-		  if (b.shouldRun()) {
-			  if(b != lastRunBehaviour) {
-				  Logger.getInstance().log("Changing to new behaviour: " + b.getName());
-				  Logger.getInstance().setStatus("Behavior: " + b.getName(),13);
-				  lastRunBehaviour.resetBehaviour();
-				  lastRunBehaviour = b;
-			  }
-			  b.doWork();
-			  break;
-		  }
-	  }
-  }
-  
-  @Override
-  public void close() throws Exception {
-	  this.sensorManager.close(); // Necessary for stopping the SensorManager.run thread.
+    behaviours.add(b);
+    Collections.sort(behaviours);
   }
 
+  private void runBehaviour() {
+    if (behaviours.size() == 0) {
+      Logger.getInstance().error("No behaviours added to the controller...");
+      return;
+    }
+
+    for (Behaviour b : behaviours) {
+      if (b.shouldRun()) {
+        if (b != lastRunBehaviour) {
+          Logger.getInstance().log("Changing to new behaviour: " + b.getName());
+          Logger.getInstance().setStatus("Behavior: " + b.getName(), 13);
+          lastRunBehaviour.resetBehaviour();
+          lastRunBehaviour = b;
+        }
+        b.doWork();
+        break;
+      }
+    }
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.sensorManager.close(); // Necessary for stopping the SensorManager.run thread.
+  }
+
+  /**
+   * Log messages to be updated on every iteration of doWork()
+   */
   private void updateStatus() {
     Logger.getInstance().setStatus("Time passed: " + Double.toString((System.currentTimeMillis() - startTime) / 1000.), 0);
     // Logger.getInstance().setStatus("Speed: " + movementManager.getSpeed(), 6);
